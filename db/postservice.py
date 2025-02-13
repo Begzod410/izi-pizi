945bd79ec4e7de9d07973b99cb9bb7f230d2f2fd
ZAYNUTDINOV Bexzod <sr.plan.spec01@AZIYABIZNES.local>
2025-02-13 09:55:22 +0500

    kjdfkjh

---
+++ db/postservice.py
@@ -0,0 +1,134 @@
+from db import get_db
+from db.models import PostPhoto, UserPost, Comment, Hashtag
+
+
+# Добавление нового поста
+def add_post_db(user_id, main_text, hashtag):
+    db = next(get_db())
+    new_post = UserPost(user_id=user_id, main_text=main_text, hashtag=hashtag)
+
+    db.add(new_post)
+    db.commit()
+    return True
+
+
+# Получение всех постов, либо одного
+def get_all_or_exact_post(post_id=0):
+    db = next(get_db())
+    if post_id:
+        all_post = db.query(UserPost).filter_by(id=post_id).first()
+        if all_post:
+            return all_post
+        return False
+    return db.query(UserPost).all()
+
+
+# Удаление поста
+def delete_post_db(post_id):
+    db = next(get_db())
+    to_delete_post = db.query(UserPost).filter_by(id=post_id).first()
+    if to_delete_post:
+        db.delete(to_delete_post)
+        db.commit()
+        return True
+    return False
+
+
+def update_post_db(post_id, new_text):
+    db = next(get_db())
+    update_post = db.query(UserPost).filter_by(id=post_id).first()
+    if update_post:
+        update_post.main_text = new_text
+        db.commit()
+        return True
+    return False
+
+
+# CRUD для комментов
+
+def add_comment_to_db(user_id, post_id, text):
+    db = next(get_db())
+    comment = Comment(user_id=user_id, post_id=post_id, comment=text)
+
+    db.add(comment)
+    db.commit()
+
+    return True
+
+
+def get_all_or_exact_comments(comment_id=0):
+    db = next(get_db())
+
+    if comment_id:
+        comment = db.query(Comment).filter_by(id=comment_id).first()
+
+        if comment:
+            return comment
+        return False
+    return db.query(Comment).all()
+
+
+def delete_comment_db(comment_id):
+    db = next(get_db())
+    comment_to_delete = db.query(Comment).filter_by(id=comment_id).first()
+
+    if comment_to_delete:
+        db.delete(comment_to_delete)
+        db.commit()
+
+        return True
+    return False
+
+
+def update_comment_db(comment_id, new_text):
+    db = next(get_db())
+    comment = db.query(Comment).filter_by(id=comment_id).first()
+
+    if comment:
+        comment.comment = new_text
+        db.commit()
+
+        return True
+    return False
+
+
+# Добавление хэштега
+def add_hashtag_db(hashtag_text):
+    db = next(get_db())
+    new_hashtag = Hashtag(hashtag_text=hashtag_text)
+
+    db.add(new_hashtag)
+    db.commit()
+    return True
+
+
+# Получение хэштега
+def get_all_or_exact_hashtags(hashtag_text=None):
+    db = next(get_db())
+    if hashtag_text:
+        exact_hashtag = db.query(Hashtag).filter_by(text=hashtag_text).first()
+        if exact_hashtag:
+            return exact_hashtag
+        return False
+    return db.query(Hashtag).all()
+
+
+# Удаление хэштега
+def delete_hashtag_db(hashtag_id):
+    db = next(get_db())
+    to_delete_hashtag = db.query(Hashtag).filter_by(id=hashtag_id).first()
+    if to_delete_hashtag:
+        db.delete(to_delete_hashtag)
+        db.commit()
+        return True
+    return False
+
+
+# Добавление фото
+def add_photo_db(post_id, photo_file):
+    db = next(get_db())
+    new_photo = PostPhoto(post_id=post_id, photo_file=photo_file)
+
+    db.add(new_photo)
+    db.commit()
+    return True
---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from api.photos import photo_router
+from api.users import user_router
+from api.hashtags import hashtag_router
+from api.comments import comment_router
+from api.posts import post_router
+from db.postservice import get_all_or_exact_post
+
+from db import Base, engine
+
+templates = Jinja2Templates(directory="templates")
+# Для создания бд
+Base.metadata.create_all(engine)
+
+app = FastAPI(docs_url="/")
+
+app.include_router(photo_router)
+app.include_router(user_router)
+app.include_router(post_router)
+app.include_router(comment_router)
+app.include_router(hashtag_router)
+
+
+@app.get("/user/{post_id}", response_class=HTMLResponse)
+async def main(request: Request, post_id: int):
+    user_post = get_all_or_exact_post(post_id)
+    return templates.TemplateResponse(name="index.html", request=request, context={"user_post": user_post})
+
+
